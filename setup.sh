90e8507182bb86c26827ebd5e3fe757c97e0a698
Jesse Crocker <jesse@DataMongers.net>
2015-09-11 11:59:23 -0600

    add setup script

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#install dependencies
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get -y install git nodejs npm supervisor nginx
+sudo ln -s /usr/bin/nodejs /usr/local/bin/node
+#copy config files into place
+sudo rsync -av elevation-service-deploy/etc/ /etc/
+
+#install elevation service
+git clone https://github.com/JesseCrocker/elevation-service.git
+cd elevation-service
+npm install
+
+#launch services
+sudo /etc/init.d/nginx restart
+sudo supervisorctl reload
+
+#test it
+curl http://localhost/status/
